2744c2732db055cab1e7988f3e8a5ad73b3dacc1
João Fernandes <joaofan11@gmail.com>
2025-10-30 12:18:54 -0400

    Atualização

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,19 +10,5 @@ module.exports.ensureAdmin = function(req, res, next) {
   if (req.session && req.session.user && req.session.user.role === 'admin') {
     return next();
   }
-  return res.status(403).render('403', { title: 'Acesso negado – Suporte TI – UNINORTE' });
-};
-module.exports.ensureAuth = function(req, res, next) {
-  if (req.session && req.session.user) {
-    res.locals.currentUser = req.session.user;
-    return next();
-  }
-  return res.redirect('/login');
-};
-
-module.exports.ensureAdmin = function(req, res, next) {
-  if (req.session && req.session.user && req.session.user.role === 'admin') {
-    return next();
-  }
   return res.status(403).render('403', { title: 'Acesso negado – Central de Suporte Técnico – UNINORTE' });
 };
